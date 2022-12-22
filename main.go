24ab9ce73c6a698a4416255cf9e754d0d11ae840
aziz-wahyudin <midnightmist37@gmail.com>
2022-12-22 15:48:33 +0700

    setup main dumy for docker build before implementing true main

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.GET("/hello", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hello world")
+	})
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+
+	e.Start(":8080")
+
+}
