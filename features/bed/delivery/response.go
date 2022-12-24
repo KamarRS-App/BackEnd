254dfa09140023776a8adb4ce4e50483f4305bfe
Shufi Ahadul <febryan.zainal@gmail.com>
2022-12-24 20:22:29 +0700

    bed entity, request, response

--- features/bed/delivery/response.go
+++ features/bed/delivery/response.go
@@ -1,1 +1,10 @@
 package delivery
+
+type BedResponse struct {
+	ID              uint   `json:"id"`
+	NamaTempatTidur string `json:"nama_tempat_tidur"`
+	Ruangan         string `json:"ruangan"`
+	Kelas           string `json:"kelas"`
+	Status          string `json:"status"`
+	HospitalID      uint   `json:"hospital_id"`
+}
