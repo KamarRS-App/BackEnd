a4c2ee61b7a6cc53d3fbe79123796fbc3f7df851
Shufi Ahadul <febryan.zainal@gmail.com>
2022-12-24 20:37:12 +0700

    bedreserve entity, request, response

--- features/bedReservation/entity.go
+++ features/bedReservation/entity.go
@@ -1,1 +1,14 @@
 package bedreservation
+
+type BedReservationCore struct {
+	ID               uint
+	StatusPasien     string
+	BiayaRegistrasi  int
+	OrderID          string
+	PaymentMethod    string
+	LinkPembayaran   string
+	StatusPembayaran string
+	HospitalID       uint
+	PatientID        uint
+	BedID            uint
+}
