45e6a14eb8980a657df2c68228cb0a365bf133d4
Shufi Ahadul <febryan.zainal@gmail.com>
2022-12-25 21:02:27 +0700

    bed mapping fix

--- features/bed/delivery/response.go
+++ features/bed/delivery/response.go
@@ -3,18 +3,19 @@ package delivery
 import "kamarRS/features/bed"
 
 type BedResponse struct {
-	ID              uint             `json:"id"`
-	NamaTempatTidur string           `json:"nama_tempat_tidur"`
-	Ruangan         string           `json:"ruangan"`
-	Kelas           string           `json:"kelas"`
-	Status          string           `json:"status"`
-	Hospital        HospitalResponse `json:"hospital"`
+	ID              uint   `json:"id"`
+	NamaTempatTidur string `json:"nama_tempat_tidur"`
+	Ruangan         string `json:"ruangan"`
+	Kelas           string `json:"kelas"`
+	Status          string `json:"status"`
+	HospitalID      uint   `json:"hospital_id"`
+	// Hospital        HospitalResponse `json:"hospital"`
 }
 
-type HospitalResponse struct {
-	ID   uint   `json:"id"`
-	Nama string `json:"nama"`
-}
+// type HospitalResponse struct {
+// 	ID   uint   `json:"id"`
+// 	Nama string `json:"nama"`
+// }
 
 // -----------------Bed--------------------------------
 func fromCore(dataCore bed.BedCore) BedResponse {
@@ -24,6 +25,7 @@ func fromCore(dataCore bed.BedCore) BedResponse {
 		Ruangan:         dataCore.Ruangan,
 		Kelas:           dataCore.Kelas,
 		Status:          dataCore.Status,
+		HospitalID:      dataCore.HospitalID,
 	}
 }
 
@@ -36,19 +38,19 @@ func fromCoreList(dataCore []bed.BedCore) []BedResponse {
 	return dataResponse
 }
 
-// -----------------Hospital--------------------------------
-func fromCore2(dataCore bed.HospitalCore) HospitalResponse {
-	return HospitalResponse{
-		ID:   dataCore.ID,
-		Nama: dataCore.Nama,
-	}
-}
+// // -----------------Hospital--------------------------------
+// func fromCore2(dataCore bed.HospitalCore) HospitalResponse {
+// 	return HospitalResponse{
+// 		ID:   dataCore.ID,
+// 		Nama: dataCore.Nama,
+// 	}
+// }
 
-// data dari core ke response
-func fromCoreList2(dataCore []bed.HospitalCore) []HospitalResponse {
-	var dataResponse []HospitalResponse
-	for _, v := range dataCore {
-		dataResponse = append(dataResponse, fromCore2(v))
-	}
-	return dataResponse
-}
+// // data dari core ke response
+// func fromCoreList2(dataCore []bed.HospitalCore) []HospitalResponse {
+// 	var dataResponse []HospitalResponse
+// 	for _, v := range dataCore {
+// 		dataResponse = append(dataResponse, fromCore2(v))
+// 	}
+// 	return dataResponse
+// }
