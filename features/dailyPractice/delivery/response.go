fdb6e7560b96c68f809f74c7a0bd912ec3ff58ca
Anti-Gen <putrategu@gmail.com>
2022-12-27 00:31:49 +0700

    Update-User

--- features/dailyPractice/delivery/response.go
+++ features/dailyPractice/delivery/response.go
@@ -1,7 +1,7 @@
 package delivery
 
 import (
-	"github.com/KamarRS-App/features/dailypractice"
+	dailypractice "github.com/KamarRS-App/features/dailyPractice"
 )
 
 type DailyPracticeResponse struct {
