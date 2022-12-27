d75ef811513201b55ef272f72f0d4e8b46aa91bf
Anti-Gen <putrategu@gmail.com>
2022-12-27 18:12:58 +0700

    Update,Delete-Patient

--- features/patient/delivery/handler.go
+++ features/patient/delivery/handler.go
@@ -19,13 +19,12 @@ func New(Service patient.ServiceInterface, e *echo.Echo) {
 		PatientService: Service,
 	}
 
+	e.GET("/patients", handler.GetAllPatient, middlewares.JWTMiddleware())
 	e.POST("/patients", handler.Create, middlewares.JWTMiddleware())
 	e.GET("/patients/:id", handler.GetByPatientId, middlewares.JWTMiddleware())
+	e.PUT("/patients/:id", handler.Update, middlewares.JWTMiddleware())
+	e.DELETE("/patients/:id", handler.DeleteById, middlewares.JWTMiddleware())
 	e.GET("/users/:id/patients", handler.GetByUserId, middlewares.JWTMiddleware())
-	// e.GET("/users", handler.GetById, middlewares.JWTMiddleware()) //untuk sementara pake param karena login belum bisa
-	// e.PUT("/users", handler.Update, middlewares.JWTMiddleware())
-	// e.DELETE("/users", handler.DeleteById, middlewares.JWTMiddleware())
-	// e.GET("/users/:id", handler.GetById, middlewares.JWTMiddleware())
 
 }
 func (delivery *PatientDeliv) Create(c echo.Context) error {
@@ -87,4 +86,52 @@ func (delivery *PatientDeliv) GetByUserId(c echo.Context) error {
 	}
 	var ResponData = ListpatientCoreTopatientRespon(result)
 	return c.JSON(http.StatusOK, helper.SuccessWithDataResponse("berhasil menampilkan patient yang di daftarkan oleh user", ResponData))
+}
+
+func (delivery *PatientDeliv) GetAllPatient(c echo.Context) error {
+
+	result, err := delivery.PatientService.GetAllPatient() //memanggil fungsi service yang ada di folder service//jika return nya 2 maka variable harus ada 2
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("erorr read data"))
+	}
+	var ResponData = ListpatientCoreTopatientRespon(result)
+	return c.JSON(http.StatusOK, helper.SuccessWithDataResponse("berhasil menampilkan semua pasien", ResponData))
+}
+
+func (delivery *PatientDeliv) Update(c echo.Context) error {
+
+	id, errConv := strconv.Atoi(c.Param("id"))
+	if errConv != nil {
+		return errConv
+	}
+
+	patientInput := RequestPatient{}
+	errBind := c.Bind(&patientInput) // menangkap data yg dikirim dari req body dan disimpan ke variabel
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error binding data "+errBind.Error()))
+	}
+
+	dataCore := patientInput.reqToCore()
+
+	err := delivery.PatientService.Update(id, dataCore)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponse("Gagal merubah data user"+err.Error()))
+	}
+	return c.JSON(http.StatusCreated, helper.SuccessResponse("Perubahan Data Berhasil"))
+}
+
+func (delivery *PatientDeliv) DeleteById(c echo.Context) error {
+
+	id, errConv := strconv.Atoi(c.Param("id"))
+	if errConv != nil {
+		return errConv
+	}
+
+	err := delivery.PatientService.DeleteById(id) //memanggil fungsi service yang ada di folder service
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("erorr Hapus data"))
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("berhasil menghapus Pasien"))
 }
