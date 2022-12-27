3d23759f9e2524ef9c66609eab707f039d4c3bc4
Anti-Gen <putrategu@gmail.com>
2022-12-28 00:23:14 +0700

    CRUD-Staff

--- features/hospitalStaff/repository/query.go
+++ features/hospitalStaff/repository/query.go
@@ -1,1 +1,123 @@
 package repository
+
+import (
+	"errors"
+	"fmt"
+
+	hospitalstaff "github.com/KamarRS-App/features/hospitalStaff"
+	"github.com/KamarRS-App/features/hospitalStaff/service"
+	"gorm.io/gorm"
+)
+
+type staffRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) hospitalstaff.RepositoryInterface { // user.repository mengimplementasikan interface repository yang ada di entities
+	return &staffRepository{
+		db: db,
+	}
+
+}
+
+// Create implements hospitalstaff.RepositoryInterface
+func (repo *staffRepository) Create(input hospitalstaff.HospitalStaffCore) (err error) {
+	var staffs []HospitalStaff
+
+	tx1 := repo.db.Find(&staffs)
+	if tx1.Error != nil {
+		return tx1.Error
+	}
+
+	for _, v := range staffs {
+		if input.Email == v.Email {
+			return errors.New("email sudah pernah terdaftar silahkan mendaftar dengan email yang lain")
+		}
+
+	}
+
+	staffGorm := FromStaffCore(input)
+
+	tx := repo.db.Create(&staffGorm) // proses insert data
+
+	if tx.Error != nil {
+		return tx.Error
+	}
+	if tx.RowsAffected == 0 {
+		return errors.New("insert failed")
+	}
+	return nil
+}
+
+// DeleteById implements hospitalstaff.RepositoryInterface
+func (repo *staffRepository) DeleteById(id int) error {
+	var staff HospitalStaff
+
+	tx1 := repo.db.Delete(&staff, id)
+	if tx1.Error != nil {
+		return tx1.Error
+	}
+
+	if tx1.RowsAffected == 0 {
+		return errors.New("id not found")
+
+	}
+
+	return nil
+}
+
+// GetStaff implements hospitalstaff.RepositoryInterface
+func (repo *staffRepository) GetStaff(id int) (data hospitalstaff.HospitalStaffCore, err error) {
+	var staff HospitalStaff
+
+	tx := repo.db.First(&staff, id)
+
+	if tx.Error != nil {
+
+		return hospitalstaff.HospitalStaffCore{}, tx.Error
+	}
+	gorms := staff.ModelsToCore()
+	return gorms, nil
+}
+
+// Update implements hospitalstaff.RepositoryInterface
+func (repo *staffRepository) Update(id int, input hospitalstaff.HospitalStaffCore) error {
+	var staffs []HospitalStaff
+
+	tx := repo.db.Find(&staffs)
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	for _, v := range staffs {
+		if input.Email == v.Email {
+			return errors.New("email sudah pernah terdaftar silahkan mendaftar dengan email yang lain")
+		}
+
+	}
+	var staff HospitalStaff
+
+	tx1 := repo.db.First(&staff, id)
+
+	if tx1.Error != nil {
+
+		return tx1.Error
+	}
+
+	if input.KataSandi == "" {
+		input.KataSandi = staff.Kata_Sandi
+	} else {
+		input.KataSandi = service.Bcript(input.KataSandi)
+
+	}
+	userGorm := FromStaffCore(input)
+
+	fmt.Println(input.KataSandi)
+	tx2 := repo.db.Model(&userGorm).Where("id = ?", id).Updates(&userGorm)
+
+	if tx2.Error != nil {
+		return tx2.Error
+	}
+
+	return nil
+}
--- features/hospitalStaff/service/logic.go
+++ features/hospitalStaff/service/logic.go
@@ -1,1 +1,78 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	hospitalstaff "github.com/KamarRS-App/features/hospitalStaff"
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type staffService struct {
+	staffRepository hospitalstaff.RepositoryInterface //data repository dri entities
+	validate        *validator.Validate
+}
+
+func New(repo hospitalstaff.RepositoryInterface) hospitalstaff.ServiceInterface { //dengan kembalian user.service
+	return &staffService{
+		staffRepository: repo,
+		validate:        validator.New(),
+	}
+}
+
+// Create implements hospitalstaff.ServiceInterface
+func (service *staffService) Create(input hospitalstaff.HospitalStaffCore) (err error) {
+	lower := strings.ToLower(input.Email)
+	input.Email = lower
+	input.Peran = "Admin"
+	generatePass := Bcript(input.KataSandi)
+	input.KataSandi = generatePass
+
+	if validateERR := service.validate.Struct(input); validateERR != nil {
+		return validateERR
+	}
+
+	errCreate := service.staffRepository.Create(input)
+	if errCreate != nil {
+		return errors.New(" Gagal membuat akun staff")
+	}
+
+	return nil
+}
+
+// DeleteById implements hospitalstaff.ServiceInterface
+func (service *staffService) DeleteById(id int) error {
+	err := service.staffRepository.DeleteById(id) // memanggil struct entities repository yang ada di entities yang berisi coding logic
+	return err
+}
+
+// GetStaff implements hospitalstaff.ServiceInterface
+func (service *staffService) GetStaff(id int) (data hospitalstaff.HospitalStaffCore, err error) {
+	data, err = service.staffRepository.GetStaff(id) // memanggil struct entities repository yang ada di entities yang berisi coding logic
+	return
+}
+
+// Update implements hospitalstaff.ServiceInterface
+func (service *staffService) Update(id int, input hospitalstaff.HospitalStaffCore) error {
+	errUpdate := service.staffRepository.Update(id, input)
+
+	if errUpdate != nil {
+		return errors.New("gagal mengupdate data , querry error")
+	}
+
+	return nil
+}
+
+func Bcript(y string) string {
+	password := []byte(y)
+
+	// Hashing the password with the default cost of 10
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(hashedPassword)
+
+}
