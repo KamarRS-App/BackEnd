39eb76854a7333875dfb89e01d3333bcd27ce2a3
aziz-wahyudin <midnightmist37@gmail.com>
2023-01-03 21:02:27 +0700

    fix bug in get all hospitals

--- features/hospital/repository/query.go
+++ features/hospital/repository/query.go
@@ -64,7 +64,7 @@ func (repo *hospitalRepository) GetAll(provinsi, kabKota, nama string, limit, of
 		totalPage = (int(count) / limit) + 1
 	}
 
-	tx := repo.db.Where("provinsi LIKE ? AND kabupaten_kota LIKE ? AND nama LIKE ?", "%"+provinsi+"%", "%"+kabKota+"%", "%"+nama+"%").Find(&hospitals)
+	tx := repo.db.Where("provinsi LIKE ? AND kabupaten_kota LIKE ? AND nama LIKE ?", "%"+provinsi+"%", "%"+kabKota+"%", "%"+nama+"%").Limit(limit).Offset(offset).Find(&hospitals)
 	if tx.Error != nil {
 		return nil, 0, tx.Error
 	}
