1492dfba40cf58ec069607962958710bdd5b49fa
Anti-Gen <putrategu@gmail.com>
2023-01-05 17:26:08 +0700

    unit_test_patient

--- features/patient/service/logic.go
+++ features/patient/service/logic.go
@@ -30,25 +30,34 @@ func (service *patientService) Create(input patient.CorePatient) (err error) {
 		return errors.New(" Kesalahan pada input data pasien")
 	}
 
-	return nil
+	return errCreate
 }
 
 // DeleteById implements patient.ServiceInterface
 func (service *patientService) DeleteById(id int) error {
-	err := service.patientRepository.DeleteById(id) // memanggil struct entities repository yang ada di entities yang berisi coding logic
+	err := service.patientRepository.DeleteById(id)
+	if err != nil {
+		return errors.New("gagal menghapus data pasien")
+	} // memanggil struct entities repository yang ada di entities yang berisi coding logic
 	return err
 }
 
 // GetByPatientId implements patient.ServiceInterface
 func (service *patientService) GetByPatientId(id int) (data patient.CorePatient, err error) {
 	data, err = service.patientRepository.GetByPatientId(id) // memanggil struct entities repository yang ada di entities yang berisi coding logic
-	return
+	if err != nil {
+		return patient.CorePatient{}, errors.New("gagal menampilkan pasien")
+	}
+	return data, err
 }
 
 // GetByUserId implements patient.ServiceInterface
 func (service *patientService) GetByUserId(pagination, limit, id int) (data []patient.CorePatient, totalpage int, err error) {
 	offset := (pagination - 1) * limit
 	data, totalpage, err = service.patientRepository.GetByUserId(limit, offset, id) // memanggil struct entities repository yang ada di entities yang berisi coding logic
+	if err != nil {
+		return nil, 0, errors.New("gagal menampilkan pasien")
+	}
 	return
 }
 
@@ -60,11 +69,14 @@ func (service *patientService) Update(id, userId int, input patient.CorePatient)
 		return errors.New("gagal mengupdate data , querry error")
 	}
 
-	return nil
+	return errUpdate
 }
 
 // GetAllPatient implements patient.ServiceInterface
 func (service *patientService) GetAllPatient() (data []patient.CorePatient, err error) {
 	data, err = service.patientRepository.GetAllPatient() // memanggil struct entities repository yang ada di entities yang berisi coding logic
-	return
+	if err != nil {
+		return nil, errors.New("gagal menampilkan pasien")
+	}
+	return data, err
 }
